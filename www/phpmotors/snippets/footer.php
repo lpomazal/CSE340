34af78471cb29428a5707d738e0b742612d22ee5
lpomazal <lpomazal@byui.edu>
2022-01-15 23:36:52 -0800

    initial commit

---
+++ www/phpmotors/snippets/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>&copy; PHP Motors, All rights reserved.</p>
+    <p>Images are belived to be "Fair Use". Notify the author if nor and they will be removed.</p>
+    <p>Last Updated: <?php echo date('j F, Y',getlastmod()); ?></p>
+</footer>
---
+++ www/phpmotors/snippets/header.php
@@ -0,0 +1,5 @@
+<div id="top-toolbar">
+    <img src="./images/logo.png" alt="PHP Motors" id="logo">
+    <a href="#" id="acc">My Account</>
+</div>
+
