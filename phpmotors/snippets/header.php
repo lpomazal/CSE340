2b3abdb13afadf648b64652c94ca77333fc193a0
lpomazal <lpomazal@byui.edu>
2022-03-13 13:48:18 -0700

    Add files via upload

---
+++ phpmotors/snippets/header.php
@@ -0,0 +1,7 @@
+<img src="/phpmotors/images/site/logo.png" alt="PHP Motors" id="logo" />
+
+<div class="headLog"><?=$_SESSION['loggedin']==true ? '<a href="/phpmotors/accounts/index.php?action=logout">Log Out</a>' : '<a href="/phpmotors/index.php?action=login">My Account</a>'; ?> 
+</div>
+
+
+
