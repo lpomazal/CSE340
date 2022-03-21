48c88ae047b5c40c785d96badc44d78bbf75976e
lpomazal <lpomazal@byui.edu>
2022-03-20 23:49:16 -0700

    Delete phpmotors directory

--- phpmotors/snippets/header.php
+++
@@ -1,4 +0,0 @@
-<img src="/phpmotors/images/site/logo.png" alt="PHP Motors" id="logo" />
-
-<div class="headLog"><?php if ($_SESSION['loggedin']==true) {echo '<a href= "/phpmotors/accounts/index.php?action=logout"><button>Log Out</button></a>';}
-else {echo '<a href="/phpmotors/index.php?action=login"><button>My Account</button></a>';}?></div>
